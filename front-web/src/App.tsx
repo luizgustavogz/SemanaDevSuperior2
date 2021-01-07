cc51b3245b482de0c7acfee720afa789e6221ce1
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2021-01-07 13:21:06 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ front-web/src/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import { ReactComponent as Logo } from './logo.svg'
+
+function Navbar() {
+    return (
+        <nav className="main-navbar">
+            <Logo />
+            <a href="home" className="logo-text">Luiz Delivery</a>
+        </nav>
+    )
+}
+
+export default Navbar;
