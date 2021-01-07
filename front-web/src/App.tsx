0f5035638372e8a8af4fd3cafbe9bdc913fefec1
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2021-01-07 13:41:18 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+import { ReactComponent as MainImage } from './main.svg';
+
+function Home() {
+    return (
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">
+                        Faça seu pedido <br /> que entregamos <br /> pra você!!!
+                    </h1>
+                    <h3 className="home-sub">
+                        Escolha o seu pedido e em poucos minutos <br /> levaremoss na sua porta
+                    </h3>
+                    <a href="orders" className="home-btn-order">
+                        FAZER PEDIDO
+                    </a>
+                </div>
+                <div className="home-image">
+                    <MainImage />
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Home;
